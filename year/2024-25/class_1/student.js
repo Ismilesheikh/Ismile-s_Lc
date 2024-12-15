6935e9f1d7bbaa7fed59803404aa8e38c40e2672
Ismilesheikh <ismiledeveloper@gmail.com>
2024-12-15 08:57:36 +0530

    ok

---
+++ year/2024-25/class_1/student.js
@@ -0,0 +1,23 @@
+let SHEET_ID = '1Dsnmnh58z9i0_vYdkzWgLAEeLfTFQcFowgHP7f90ZL0'
+let SHEET_TITLE = 'Class-X';
+let SHEET_RANGE = 'C12:H30';
+
+let FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE);
+
+fetch(FULL_URL)
+.then(res => res.text())
+.then(rep => {
+    let data = JSON.parse(rep.substr(47).slice(0,-2));
+
+    let name = document.getElementById('name1');
+    let student_age = document.getElementById('age');
+    let name1 = document.getElementById('name1');
+    let age2 = document.getElementById('age2');
+    let length = data.table.rows.length;
+
+    console.log(data.table.rows);
+ 
+  
+
+ 
+})
