f7b40d00f81b98f73810a37172042e9a3360b81d
Ismilesheikh <ismiledeveloper@gmail.com>
2024-12-16 21:38:08 +0530

    kn

--- year/2024-25/class_1/student.js
+++ year/2024-25/class_1/student.js
@@ -1,22 +1,18 @@
-let SHEET_ID = '1Dsnmnh58z9i0_vYdkzWgLAEeLfTFQcFowgHP7f90ZL0'
-let SHEET_TITLE = 'Class-X';
-let SHEET_RANGE = 'C7:H13';
 
-let FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE);
-
-fetch(FULL_URL)
-.then(res => res.text())
-.then(rep => {
-    let data = JSON.parse(rep.substr(47).slice(0,-2));
-
-    let name = document.getElementById('name1');
-    let pname = document.getElementById('pname');
-    let dob = document.getElementById('dob1');
-    let mno = document.getElementById('mno1');
-    let length = data.table.rows.length;
-
- name.innerHTML=(data.table.rows[0].c[1]).v;
-  
-
- 
-})
+$(document).ready(function() {
+    function update_data() {
+        $.getJSON("https://example.com/example.json", function(Data) {
+                    
+            // Example 1
+            document.getElementById('name1').innerHTML = Data.example1;
+                            
+            // Example 2
+            document.getElementById('example2').innerHTML = Data.example2;
+                    
+            // Example 3
+            document.getElementById('example3').innerHTML = Data.example3;
+        });
+    }
+update_data();
+setInterval(update_data, '30000');
+});
