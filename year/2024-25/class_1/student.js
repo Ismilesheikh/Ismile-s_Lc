b1fbc0e64382cc55636341af3f3a21f22cf5902f
Ismilesheikh <100894383+Ismilesheikh@users.noreply.github.com>
2024-12-16 22:31:04 +0530

    Update student.js

---
+++ year/2024-25/class_1/student.js
@@ -0,0 +1,28 @@
+async function getUsers() {
+  try {
+    const response = await fetch(
+      'link---   https://script.google.com/macros/s/AKfycbz3nfDScHLs3mqhCv_iW5fG3g3qSVlz7P4xfYGdWsTQpAS6aw0OTW2ErX1_iBXdzKVh/exec',
+      {
+        method: 'GET',
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getUsers().then(data => {
+  console.log(data);
+
+  const preElement = document.getElementById('name');
+
+  preElement.innerHTML = data[0].D;
+});
