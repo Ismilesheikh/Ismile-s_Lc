761ebb32609cc1adcccfa5e9a8d7a8a9e1ad0577
Ismilesheikh <100894383+Ismilesheikh@users.noreply.github.com>
2024-12-18 11:34:21 +0530

    Update student.js

--- year/2024-25/class_1/student.js
+++ year/2024-25/class_1/student.js
@@ -1,3 +1,18 @@
+//Password Protected 
+function fun(){
+let correctPassword ="Ismile";
+let Password=document.getElementById("psw").value;
+if(correctPassword===Password){
+    document.getElementById("students").style.display="block";
+    document.getElementById("validate").style.display="none";
+}
+else{alert("enter correct password");}
+
+
+}
+
+//code for Google sheets 
+
 async function getUsers() {
   try {
     const response = await fetch(
