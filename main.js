05e74c3b0f62083afa35f874b52873c44467531a
Ismilesheikh <100894383+Ismilesheikh@users.noreply.github.com>
2024-12-31 20:28:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ let hour=parseInt( (Number(x)-Number(y))/(1000*60*60));
 let min =parseInt( (Number(x)-Number(y))/(1000*60))-hour*60;
 let sec =parseInt( (Number(x)-Number(y))/(1000))-hour*60*60-min*60;
 
-document.getElementById('counter').innerHTML=The Hours:+ hour+ ':'+ min +':'+sec;
+document.getElementById('counter').innerHTML='The Hours:'+ hour+ ':'+ min +':'+sec;
