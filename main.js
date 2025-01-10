9e4cd0dd5865457fc808657b68046a6eee7a2bb1
Ismilesheikh <100894383+Ismilesheikh@users.noreply.github.com>
2025-01-10 12:20:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ function UpdateTime() {
 
 let x = new Date();
 let y = new Date("2025-01-10 10:17:00");
-let hour=parseInt( (Number(x)-Number(y))/(1000*60*60));
-let min =parseInt( (Number(x)-Number(y))/(1000*60))-hour*60;
-let sec =parseInt( (Number(x)-Number(y))/(1000))-hour*60*60-min*60;
+let hour=`0${parseInt( (Number(x)-Number(y))/(1000*60*60))}`.slice(-2);
+let min =`0${parseInt( (Number(x)-Number(y))/(1000*60))-hour*60}`.slice(-2);
+let sec =`0${parseInt( (Number(x)-Number(y))/(1000))-hour*60*60-min*60}`.slice(-2);
 if(hour>7*24){document.getElementById('counter').innerHTML="It's Time to Cut You Hairs";}
 else
 {document.getElementById('counter').innerHTML='The Hours:'+ hour+ ':'+ min +':'+sec;};
