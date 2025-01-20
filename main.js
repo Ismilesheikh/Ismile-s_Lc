2b5ebccac369e40b4a99613ab45812bba53e07f3
Ismilesheikh <100894383+Ismilesheikh@users.noreply.github.com>
2025-01-20 09:48:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let y = new Date("2025-01-17 00:00:00");
 let hour=parseInt( (Number(x)-Number(y))/(1000*60*60));
 let min =parseInt( (Number(x)-Number(y))/(1000*60))-hour*60;
 let sec =parseInt( (Number(x)-Number(y))/(1000))-hour*60*60-min*60;
-
+let days=parseInt(hour/24);
 if(hour.toString().length<2){
 hours=`0${hour}`.slice(-2);
 }
@@ -32,7 +32,7 @@ else{secs=sec;}
 if(hour>7*24){document.getElementById('counter').innerHTML="It's Time to Cut You Hairs";}
 else
 {document.getElementById('counter').innerHTML='The Hours:'+ hours+ ':'+ mins +':'+secs;};
-document.getElementById('days').innerHTML='Days:'+' '+parseInt(hour/24);
+document.getElementById('days').innerHTML='Days=>>Hour:'+' '+days+ hour-days*24;
 counter.style.color='green';};
 
 setInterval(UpdateTime,1000);
