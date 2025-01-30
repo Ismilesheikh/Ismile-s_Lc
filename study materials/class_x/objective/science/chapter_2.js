e2f64e92816e16e20e32da463ce188511e94f02b
Ismilesheikh <100894383+Ismilesheikh@users.noreply.github.com>
2025-01-30 15:40:47 +0530

    Update chapter_2.js

--- study materials/class_x/objective/science/chapter_2.js
+++ study materials/class_x/objective/science/chapter_2.js
@@ -19,7 +19,7 @@
   }
 }
 
-function createQuestionElement(questionId, questionText, answers) {
+function createQuestionElement(questionId, questionText, answers,imgLinks) {
   const queAnsDiv = document.createElement('div');
   queAnsDiv.className = 'que_ans';
 
@@ -28,12 +28,19 @@ function createQuestionElement(questionId, questionText, answers) {
   const questionP = document.createElement('p');
   questionP.id = questionId;
   questionP.innerText = questionText;
+  const imgDiv=document.createElement('div');
+  imgDiv.className='imgDiv';
+  const image=document.createElement('img');
+  image.id=`${questionId}_img`;
+  image.src=imgLinks;
+  image.alt='';
+
   const snoP = document.createElement('p');
  snoP.className = 'sno';
   snoP.innerText = `Q${questionId.slice(1)}.`;
   questionDiv.appendChild(snoP);
   questionDiv.appendChild(questionP);
-
+imgDiv.appendChild(image);
   const answersDiv = document.createElement('div');
   answersDiv.className = 'ans';
 
@@ -45,6 +52,7 @@ function createQuestionElement(questionId, questionText, answers) {
   });
 
   queAnsDiv.appendChild(questionDiv);
+  queAnsDiv.appendChild(imgDiv);
   queAnsDiv.appendChild(answersDiv);
 
   // Create and append the custom button
@@ -56,26 +64,34 @@ function createQuestionElement(questionId, questionText, answers) {
 
   return queAnsDiv;
 }
+let img_links=['','','','','https://th.bing.com/th/id/R.c7e89b930f8aa73cf72d5f167d1d16ed?rik=3obl%2fhI6QdNIMA&riu=http%3a%2f%2fthewowstyle.com%2fwp-content%2fuploads%2f2015%2f01%2ffree-beautiful-place-wallpaper-hd-173.jpg&ehk=92RRpT4hrYheMDBZkK0HhLLXx9%2fGDjnafeDmbgjE1K8%3d&risl=&pid=ImgRaw&r=0']
+
 
 getUsers().then(data => {
   if (data && data.length > 0) {
+    // Set chapter name
+let chName=document.getElementById("chname");
+let picContainer=document.getElementById("pPic");
+    chName.innerHTML = data[1].B7;
+const chPic =document.createElement('img');
+chPic.src=data[1].Q2;
+chPic.className='chPic';
+picContainer.appendChild(chPic);
 
     let totalQuestion =Number(data[2].Answer2);
-    // Set chapter name
-    document.getElementById("chName").innerHTML = data[1].B2;
+
 
     // Generate questions and answers
     const questionsContainer = document.getElementById('questionsContainer');
     for (let i = 1; i <=totalQuestion; i++) {
-
-      const questionText = data[2 * i + 1].Q2;
+      const questionText = data[2 * i + 1].Q7;
       const answers = [
         data[2 * i + 2].A2,
         data[2 * i + 2].B2,
         data[2 * i + 2].C2,
         data[2 * i + 2].D2,
       ];
-      const questionElement = createQuestionElement(`q${i}`, questionText, answers);
+      const questionElement = createQuestionElement(`q${i}`, questionText, answers,`${data[2*i+1].D2}`);
       questionsContainer.appendChild(questionElement);
     }
 
@@ -103,7 +119,7 @@ function sum(rightAnswer) {
   return correctAnswers.push(rightAnswer);
 }
 for(let i=1; i<=totalQuestion;i++){
-  rightAnswer=sum(Number(data[2*i+2].Answer6));
+  rightAnswer=sum(Number(data[2*i+2].Answer7));
 }
 
 
@@ -135,8 +151,14 @@ document.getElementById(`q${i}_ans${correctAnswerIndex}`).disabled = true;
   });
 
 
-}
+
 
 }
+
+
+
+
+}
+
 
 });
